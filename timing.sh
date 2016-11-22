4a07bb0ab11b9c3528ea51cd0e548703d64f99f8
Alexander Kruppa <akruppa@gmail.com>
2016-11-22 14:53:32 +0100

    Time various alignments

--- timing.sh
+++ timing.sh
@@ -1,16 +1,42 @@
+#!/usr/bin/env bash
 
 # original sequence: 199.000000 (pmc0: 3682011, pmc1: 0, pmc2: 0, pmc3: 0)
 
 declare -a TIMES
 
-for L in `seq 50`
+if [ -f timing_config.sh ]
+then
+  source timing_config.sh
+fi
+
+COMMENT="#"
+for L in `seq 100`
 do
-  for I in 1 2 3
+  LINE="$L"
+  for A1 in 0 8 16 24
   do
-    TIMES[$I]="`~/ajs-jev -m 1 -l "$L" "$@" | grep "# original sequence" | cut -d " " -f 4 | cut -d . -f 1`"
+    for A2 in 0 8 16 24
+    do
+      ALIGNMENT="$A1,$A2,0,0"
+      if [ -n "$COMMENT" ]
+      then
+        COMMENT="$COMMENT $ALIGNMENT"
+      fi
+      for I in 1 2 3
+      do
+        TIMES[$I]="`~/ajs-jev $CPUBIND -m 1 -l "$L" -d "$ALIGNMENT" "$@" | grep "# original sequence" | cut -d " " -f 4 | cut -d . -f 1`"
+      done
+      # Sort the timings into increasing order
+      if [ "${TIMES[1]}" -gt "${TIMES[2]}" ]; then T="${TIMES[1]}"; TIMES[1]="${TIMES[2]}"; TIMES[2]="$T"; fi
+      if [ "${TIMES[2]}" -gt "${TIMES[3]}" ]; then T="${TIMES[2]}"; TIMES[2]="${TIMES[3]}"; TIMES[3]="$T"; fi
+      if [ "${TIMES[1]}" -gt "${TIMES[2]}" ]; then T="${TIMES[1]}"; TIMES[1]="${TIMES[2]}"; TIMES[2]="$T"; fi
+      LINE="$LINE ${TIMES[2]}"
+    done
   done
-  if [ "${TIMES[1]}" -gt "${TIMES[2]}" ]; then T="${TIMES[1]}"; TIMES[1]="${TIMES[2]}"; TIMES[2]="$T"; fi
-  if [ "${TIMES[2]}" -gt "${TIMES[3]}" ]; then T="${TIMES[2]}"; TIMES[2]="${TIMES[3]}"; TIMES[3]="$T"; fi
-  if [ "${TIMES[1]}" -gt "${TIMES[2]}" ]; then T="${TIMES[1]}"; TIMES[1]="${TIMES[2]}"; TIMES[2]="$T"; fi
-  echo "$L ${TIMES[2]}"
+  if [ -n "$COMMENT" ]
+  then
+    echo "$COMMENT"
+    unset COMMENT
+  fi
+  echo "$LINE"
 done
